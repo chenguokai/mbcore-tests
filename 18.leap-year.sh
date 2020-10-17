1d558b27ea46510a17416db2cbeeb1572dd9f2d0
Chen Guokai <chenguokai17@mails.ucas.ac.cn>
2020-10-18 00:22:45 +0800

    Push to public

---
+++ 18.leap-year.sh
@@ -0,0 +1,3 @@
+../riscv64-unknown-elf-gcc-8.3.0-2020.04.0-x86_64-apple-darwin/bin/riscv64-unknown-elf-objcopy -O binary /Users/cgk/Downloads/prj4-cebarobot-master/benchmark/advanced/bin/leap-year   ../test.bin.ori
+
+../riscv64-unknown-elf-gcc-8.3.0-2020.04.0-x86_64-apple-darwin/bin/riscv64-unknown-elf-objcopy -I binary -O binary --reverse-bytes=4 ../test.bin.ori ../test.bin
